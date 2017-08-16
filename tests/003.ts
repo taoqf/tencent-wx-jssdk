4555a275eebb7f033e63f9f61d603e69c64b736b
taoqf <tao_qiufeng@126.com>
2017-08-16 09:25:54 +0800

    add test

---
+++ tests/003.ts
@@ -0,0 +1,21 @@
+import wx = require('tencent-wx-jssdk');
+
+wx.config({
+	debug: true,
+	appId: "",
+	timestamp: 1,
+	nonceStr: "",
+	signature: "",
+	jsApiList: []
+});
+
+import { config } from 'tencent-wx-jssdk';
+
+config({
+	debug: true,
+	appId: "",
+	timestamp: 1,
+	nonceStr: "",
+	signature: "",
+	jsApiList: []
+});
